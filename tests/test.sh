7b5ae8d99a784e3c2502ba4b438a565c39614361
Oleg Doronin <dorooleg@yandex.ru>
2017-03-25 14:17:08 +0300

    Double && delimiter improvement

--- tests/test.sh
+++ tests/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 VALUE="$(../bin/lexer.sh base_test)"
-if [[ "$VALUE" == 'KW_Read(0, 0, 3); Ident("x", 0, 5, 5); Colon(0, 6, 6); KW_If(0, 8, 9); Ident("y", 0, 11, 11); Op(PLUS, 0, 13, 13); Num(1, 0, 15, 15); Op(EQ, 0, 17, 18); Ident("x", 0, 20, 20); KW_Then(0, 22, 25); KW_Write(0, 27, 31); Ident("y", 0, 33, 33); KW_Else(0, 35, 38); KW_Write(0, 40, 44); Ident("x", 0, 46, 46); ' ]]; then
+if [[ "$VALUE" == 'KW_Read(0, 0, 3); Ident("x", 0, 5, 5); Semicolon(0, 6, 6); KW_If(0, 8, 9); Ident("y", 0, 11, 11); Op(PLUS, 0, 13, 13); Num(1, 0, 15, 15); Op(EQ, 0, 17, 18); Ident("x", 0, 20, 20); KW_Then(0, 22, 25); KW_Write(0, 27, 31); Ident("y", 0, 33, 33); KW_Else(0, 35, 38); KW_Write(0, 40, 44); Ident("x", 0, 46, 46); ' ]]; then
     echo "base_test: done"
 else
     echo "base_test: failed"
@@ -48,9 +48,25 @@ else
 fi
 
 VALUE="$(../bin/lexer.sh full)"
-if [[ "$VALUE" == 'KW_If(0, 0, 1); KW_Then(0, 3, 6); KW_Else(0, 8, 11); KW_While(0, 13, 17); KW_Do(0, 19, 20); KW_Read(0, 22, 25); KW_Write(0, 27, 31); KW_Begin(0, 33, 37); KW_End(0, 39, 41); Op(PLUS, 1, 0, 0); Op(MINUS, 1, 2, 2); Op(MUL, 1, 4, 4); Op(DIV, 1, 6, 6); Op(MOD, 1, 8, 8); Op(EQ, 1, 10, 11); Op(NEQ, 1, 13, 14); Op(GT, 1, 16, 16); Op(GTE, 1, 18, 19); Op(LT, 1, 21, 21); Op(LTE, 1, 23, 24); Op(AND, 1, 26, 27); Op(OR, 1, 29, 30); Colon(2, 0, 0); Colon(2, 2, 2); Colon(2, 4, 4); Comment(hello world, 3, 0, 12); Ident("variable", 5, 0, 7); Ident("_var", 6, 0, 3); Ident("v123b", 7, 0, 4); Bool(TRUE, 8, 0, 3); Bool(FALSE, 9, 0, 4); ' ]]; then
+if [[ "$VALUE" == 'KW_If(0, 0, 1); KW_Then(0, 3, 6); KW_Else(0, 8, 11); KW_While(0, 13, 17); KW_Do(0, 19, 20); KW_Read(0, 22, 25); KW_Write(0, 27, 31); KW_Begin(0, 33, 37); KW_End(0, 39, 41); Op(PLUS, 1, 0, 0); Op(MINUS, 1, 2, 2); Op(MUL, 1, 4, 4); Op(DIV, 1, 6, 6); Op(MOD, 1, 8, 8); Op(EQ, 1, 10, 11); Op(NEQ, 1, 13, 14); Op(GT, 1, 16, 16); Op(GTE, 1, 18, 19); Op(LT, 1, 21, 21); Op(LTE, 1, 23, 24); Op(AND, 1, 26, 27); Op(OR, 1, 29, 30); LeftBracket(2, 0, 0); RightBracket(2, 2, 2); Semicolon(2, 4, 4); Comment(hello world, 3, 0, 12); Ident("variable", 5, 0, 7); Ident("_var", 6, 0, 3); Ident("v123b", 7, 0, 4); Bool(TRUE, 8, 0, 3); Bool(FALSE, 9, 0, 4); ' ]]; then
     echo "full: done"
 else
     echo "full: failed"
+    echo "$VALUE"
+fi
+
+VALUE="$(../bin/lexer.sh simple)"
+if [[ "$VALUE" == 'KW_Write(0, 0, 4); LeftBracket(0, 6, 6); Num(1, 0, 7, 7); Op(PLUS, 0, 8, 8); Ident("x", 0, 9, 9); RightBracket(0, 10, 10); ' ]]; then
+    echo "simple: done"
+else
+    echo "simple: failed"
+    echo "$VALUE"
+fi
+
+VALUE="$(../bin/lexer.sh common)"
+if [[ "$VALUE" == 'KW_If(0, 0, 1); KW_Then(0, 3, 6); KW_Else(0, 8, 11); KW_While(1, 0, 4); KW_Do(1, 6, 7); KW_Begin(2, 0, 4); KW_End(2, 8, 10); KW_Read(3, 0, 3); KW_Write(3, 7, 11); Bool(TRUE, 4, 0, 3); Bool(FALSE, 4, 5, 9); Op(PLUS, 5, 0, 0); Op(MINUS, 5, 2, 2); Op(MUL, 5, 4, 4); Op(DIV, 5, 6, 6); Op(MOD, 5, 8, 8); Op(EQ, 5, 10, 11); Op(NEQ, 5, 13, 14); Op(GT, 5, 16, 16); Op(GTE, 5, 18, 19); Op(LT, 5, 21, 21); Op(LTE, 5, 23, 24); Op(AND, 5, 26, 27); Op(OR, 5, 29, 30); LeftBracket(6, 0, 0); RightBracket(6, 2, 2); Semicolon(7, 1, 1); Num(1, 8, 0, 0); Num(1, 9, 0, 2); Num(-inf, 10, 0, 12); Comment( this is a comment, 11, 0, 19); ' ]]; then
+    echo "common: done"
+else
+    echo "common: failed"
     echo "$VALUE"
 fi
